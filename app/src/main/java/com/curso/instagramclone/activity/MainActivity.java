e10ac4d1ef6dee3c8444a4279f42dd8d8daf9ec8
sandro <sandrorogerio.brandao@gmail.com>
2020-11-25 19:15:44 -0400

    Implementada inicialmente a Bottom Navigation:  _ criada (menu com icones)  _ configurada (animacao de icones e transicao de fragments)

--- app/src/main/java/com/curso/instagramclone/activity/MainActivity.java
+++ app/src/main/java/com/curso/instagramclone/activity/MainActivity.java
@@ -3,6 +3,9 @@ package com.curso.instagramclone.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,8 +14,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.curso.instagramclone.R;
+import com.curso.instagramclone.fragment.FeedFragment;
+import com.curso.instagramclone.fragment.PerfilFragment;
+import com.curso.instagramclone.fragment.PesquisaFragment;
+import com.curso.instagramclone.fragment.PostagemFragment;
 import com.curso.instagramclone.helper.ConfiguracaoFirebase;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth autenticacao;
@@ -29,6 +38,57 @@ public class MainActivity extends AppCompatActivity {
 
         // configuracao inicial de objetos
         autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        // configura bottom navigation view
+        configuraBottomNavigationView();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace( R.id.viewPager, new FeedFragment()).commit();
+    }
+
+    public void configuraBottomNavigationView() {
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById( R.id.bottomNavigation );
+
+        // configuracoes iniciais
+        bottomNavigationViewEx.enableAnimation( true );
+        bottomNavigationViewEx.enableItemShiftingMode( false );
+        bottomNavigationViewEx.enableShiftingMode( false );
+        bottomNavigationViewEx.setTextVisibility( false );
+
+        // habilitar a navegacao
+        habilitarNavegacao( bottomNavigationViewEx );
+
+        // configura item selecionado inicialmente
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(0 ); // define Home como padrao
+        menuItem.setChecked( true );
+    }
+
+    public void habilitarNavegacao( BottomNavigationViewEx viewEx ) {
+        viewEx.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected( @NonNull MenuItem item ) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                switch( item.getItemId()) {
+                    case R.id.ic_home :
+                        fragmentTransaction.replace( R.id.viewPager, new FeedFragment()).commit();
+                        return true;
+                    case R.id.ic_pesquisa :
+                        fragmentTransaction.replace( R.id.viewPager, new PesquisaFragment()).commit();
+                        return true;
+                    case R.id.ic_postagem :
+                        fragmentTransaction.replace( R.id.viewPager, new PostagemFragment()).commit();
+                        return true;
+                    case R.id.ic_perfil :
+                        fragmentTransaction.replace( R.id.viewPager, new PerfilFragment()).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/curso/instagramclone/fragment/PostagemFragment.java
@@ -0,0 +1,24 @@
+package com.curso.instagramclone.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.curso.instagramclone.R;
+
+public class PostagemFragment extends Fragment {
+    public PostagemFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_postagem, container, false);
+    }
+}
